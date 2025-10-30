a1bf958df2377c1e21720b98f7795e6c2b242729
JuanT05 <jtsebastiantarazona@gmail.com>
2025-10-30 01:33:11 -0500

    Endpoints autor

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from fastapi import FastAPI
-from database import create_db_and_tables
+from fastapi import FastAPI, Depends, HTTPException
+from sqlmodel import Session
+from database import create_db_and_tables, get_session
+from models import Autor
+from schemas import AutorCreate, AutorRead
+from crud import crear_autor, listar_autores
+from typing import List
 
 app = FastAPI(title="Sistema de Gestión de Biblioteca")
 
@@ -7,6 +12,23 @@ app = FastAPI(title="Sistema de Gestión de Biblioteca")
 def on_startup():
     create_db_and_tables()
 
+
 @app.get("/")
 def read_root():
     return {"mensaje": "Bienvenido al Sistema de Gestión de Biblioteca 📚"}
+
+
+# -------------------------
+# ENDPOINTS DE AUTORES
+# -------------------------
+
+@app.post("/autores/", response_model=AutorRead)
+def crear_autor_endpoint(autor: AutorCreate, session: Session = Depends(get_session)):
+    nuevo_autor = Autor.from_orm(autor)
+    return crear_autor(session, nuevo_autor)
+
+
+@app.get("/autores/", response_model=List[AutorRead])
+def listar_autores_endpoint(pais: str | None = None, session: Session = Depends(get_session)):
+    autores = listar_autores(session, pais)
+    return autores
